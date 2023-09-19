5fea7e36b16805eb647f2b2ad1e0f9f2e3a517be
tunilo <145199884+tunilo@users.noreply.github.com>
2023-09-19 23:50:11 +0400

    Add files via upload

---
+++ Assignment1/index.js
@@ -0,0 +1,33 @@
+// პირველი სავარჯიშო
+let num = 100;
+if (num > 50) {
+console.log("more than 50")
+} else
+if (num  < 20 )
+console.log("less than 20");
+else console.log("Error");
+
+//მეორე
+let name = "mariam";
+if(name == 'mariam') console.log(true);
+else console.log(false);
+
+//მესამე
+switch(name){
+    case 'mariam': console.log(true)
+        break;
+   default: console.log(false);
+} 
+//მეოთხე
+let any=true;
+console.log(typeof(any));
+
+//მეხუთე
+
+let number = 10;
+let number1 = number.toString();
+
+console.log(number1);
+
+
+
